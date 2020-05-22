49909daef5e71f08fd8ce59fb6b06424e461cad5
Kunihiro Kuroda <kunihiro.kuroda@gmail.com>
2020-05-22 09:10:29 +0900

    add: store => 都道府県別の人口推移データ取得・格納処理

--- store/index.js
+++ store/index.js
@@ -1,16 +1,31 @@
 export const state = () => ({
-  pref: []
+  pref: [],
+  population: []
 })
 
 export const mutations = {
   setPref(state, data) {
     state.pref = data
+  },
+  setPopulation(state, data) {
+    const obj = {
+      prefCode: data.prefCode,
+      prefName: data.prefName,
+      data: data.result.data[0].data
+    }
+    state.population.push(obj)
   }
 }
 
 export const getters = {
   getPref: (state) => {
     return state.pref
+  },
+  existsPopulation: (state) => (prefCode) => {
+    const index = state.population.findIndex(
+      (item) => item.prefCode === prefCode
+    )
+    return index !== -1
   }
 }
 
@@ -18,5 +33,19 @@ export const actions = {
   async fetchPref({ commit }) {
     const data = await this.$api.$get('/prefectures')
     if (data.result) commit('setPref', data.result)
+  },
+
+  async fetchPopulation({ commit, getters }, prefCode) {
+    if (getters.existsPopulation(prefCode)) {
+      return false
+    }
+
+    const data = await this.$api.$get(
+      `/population/composition/perYear?prefCode=${prefCode}`
+    )
+
+    if (data.result) {
+      commit('setPopulation', { result: data.result, prefCode })
+    }
   }
 }
